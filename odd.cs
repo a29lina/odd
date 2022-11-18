00d6b521bef38f1a2c3b5e458da34c3f44c97230
Alina <alishad29@mail.ru>
2022-11-19 00:29:39 +0300

    odd

---
+++ odd.cs
@@ -0,0 +1,22 @@
+﻿int[] array = {7, 4, 6, 9, 10, 12, 18};
+int n = array.Length;
+
+
+int count = 0;
+int index = 0;
+
+while (index < n)
+{
+    if(array[index]%2==1)
+    {
+        count ++;
+        index ++;
+    }
+    else
+    {
+        index ++;
+    }
+
+    index = index ++;
+}
+    Console.WriteLine(count);
